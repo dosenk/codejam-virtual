6307e8fabc9cb0c3438e2845de9af8977aec46e8
Dmitry Osipenko <eympel@gmail.com>
2020-04-07 22:16:40 +0300

    feat: completed the task

--- src/app.js
+++ src/app.js
@@ -1,29 +1,11 @@
-import Keybord from './moduls/class.js';
-import {
-  noRepeatKeys, lang, KEY_CODE, KEYS,
-} from './moduls/CONST.js';
-
-const addMainConteiner = () => {
-  const mainDiv = Keybord.createButton('div', 'wrapper');
-  const textAreaDiv = Keybord.createButton('div', 'textArea');
-  let textArea = Keybord.createButton('textarea');
-  textArea.setAttribute('autofocus', '');
-  textArea.setAttribute('cols', '30');
-  textArea.setAttribute('rows', '10');
-  textArea.onblur = () => textArea.focus();
-  const keydbord = Keybord.createButton('div', 'keydbord-wrapper');
-  const infoDiv = Keybord.createButton('div', 'info');
-  const infoDivText = Keybord.createButton('p', 'infoText');
-  infoDivText.innerText = 'Клавиатура создана в операционной системе Windows \t\n Для переключения языка комбинация: левыe Shift + Alt';
-  infoDiv.append(infoDivText);
-  textAreaDiv.append(textArea);
-  mainDiv.append(textAreaDiv, keydbord, infoDiv);
-  document.body.append(mainDiv);
-};
+import Keybord from './class.js';
+import {UPPERKEY, KEY_CODE, KEYS} from './const.js';
 
 window.onload = () => {
-  addMainConteiner();
-  let lang1 = Keybord.checkLocalstorage(localStorage);
-  const keybord = new Keybord(lang1.lang, KEYS, KEY_CODE, noRepeatKeys);
-  keybord.addListenersOnKeys();
+    document.body.innerHTML = "";
+    Keybord.checkLocalstorage();
+    let capsLock = localStorage.capsLock === '1' ? true : false;
+    const keybord = new Keybord(localStorage.lang, capsLock, KEYS, KEY_CODE, UPPERKEY);
+    keybord.renderKeybord();
+    keybord.addListenersOnKeys();
 };
